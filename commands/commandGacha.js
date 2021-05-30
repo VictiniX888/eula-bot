5932c246c1afed905cc77920a53deee2c4a681e5
VictiniX888 <victiniy@gmail.com>
2021-05-30 21:34:13 +0800

    Ensure firebase only initializes once

--- commands/commandGacha.js
+++ commands/commandGacha.js
@@ -22,13 +22,15 @@ const options = [
 ];
 
 function initFirebaseDb() {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-  });
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
 
   return admin.firestore();
 }
